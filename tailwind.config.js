164dce1864096f03e89552b8b4648ac782658ec2
kmrakash <aksh.iem22@gmail.com>
2021-06-18 18:17:29 +0530

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
-    './src/**/*.js',
+    './index.html',
+    './src/**/*.{react, js, ts, jsx, tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
